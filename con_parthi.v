88e657dba8e25ab49b9bf3fe47609c6d2d2bb32e
solvex123 <66999501+solvex123@users.noreply.github.com>
2020-07-05 20:54:20 +0530

    Add files via upload

---
+++ con_parthi.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.06.2020 13:00:35
+// Design Name: 
+// Module Name: con_parthi
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module con_parthi();
+    
+`define x 4
+`define y 2
+
+endmodule
